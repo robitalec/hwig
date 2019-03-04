dff2d32cd4106f6b73755c643e9df3b9815af9e6
Alec Robitaille <robit.alec@gmail.com>
2019-03-04 15:02:46 -0800

    add basic tests

--- tests/testthat/test-hwi.R
+++ tests/testthat/test-hwi.R
@@ -1,5 +1,14 @@
 context("test-hwi")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+data(DT)
+
+test_that("hwi works", {
+	expect_true(inherits(calc_hwi(DT, 'id', 'group', by = NULL), 'data.table'))
+
+	expect_equal(length(calc_hwi(DT, 'id', 'group', by = 'yr')),
+							 DT[, uniqueN(yr)])
+})
+
+
+test_that("hwig works", {
 })
