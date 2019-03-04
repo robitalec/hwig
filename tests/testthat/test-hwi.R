85bae920ee2aab6684c5e18e58c3ac30fc6fb993
Alec Robitaille <robit.alec@gmail.com>
2019-03-04 15:31:37 -0800

    add hwig tests

--- tests/testthat/test-hwi.R
+++ tests/testthat/test-hwi.R
@@ -2,13 +2,20 @@ context("test-hwi")
 
 data(DT)
 
-test_that("hwi works", {
-	expect_true(inherits(calc_hwi(DT, 'id', 'group', by = NULL), 'data.table'))
+single <- calc_hwi(DT, 'id', 'group', by = NULL)
+multiple <- calc_hwi(DT, 'id', 'group', by = 'yr')
 
-	expect_equal(length(calc_hwi(DT, 'id', 'group', by = 'yr')),
-							 DT[, uniqueN(yr)])
+
+test_that("hwi works", {
+	expect_true(inherits(single, 'data.table'))
+
+	expect_equal(length(multiple), DT[, uniqueN(yr)])
 })
 
 
 test_that("hwig works", {
+
+		expect_true(inherits(calc_hwig(single), 'data.table'))
+
+		expect_equal(length(calc_hwig(multiple)), DT[, uniqueN(yr)])
 })
