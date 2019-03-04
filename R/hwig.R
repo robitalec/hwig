6e7655c282a39b116252c9b61df5699e77b5f244
Alec Robitaille <robit.alec@gmail.com>
2019-03-04 14:02:01 -0800

    fst hwig

--- R/hwig.R
+++ R/hwig.R
@@ -9,7 +9,7 @@
 #' @export
 #'
 #' @examples
-CalculateHWI <- function(ind.groups, input.data.format = 'individuals',
+calc_hwi <- function(ind.groups, input.data.format = 'individuals',
 												 network.data.format = 'GBI', assc.ind = 'HWI'){
 	if (!'ID' %in% colnames(ind.groups)) {
 		warning("Column named 'ID' not found, ensure that an ID column is present in the input data")
@@ -35,4 +35,31 @@ CalculateHWI <- function(ind.groups, input.data.format = 'individuals',
 	hwi.full <- data.table(get_network(memb.matrix, data_format = network.data.format,
 																		 association_index = assc.ind))
 	return(hwi.full)
+}
+
+
+#' @param hwi.dt output of `calc_hwi``
+#'
+#' @return HWIG matrix
+#' @export
+#'
+#' @examples
+calc_hwig <- function(hwi.dt){
+	hwi.sums <- melt(hwi.dt[, lapply(.SD, sum), .SDcols = colnames(hwi.dt)],
+									 measure.vars = colnames(hwi.dt),
+									 variable.name = 'ID', value.name = 'HWI')
+
+	hwi.sums[, hwiTotal := sum(HWI)/2]
+	hwi.grand.total <- hwi.sums[1, hwiTotal]
+
+	hwi.mult.inds <- data.table(hwi.sums[, outer(HWI, HWI, '*')])
+
+	hwi.div <- hwi.mult.inds[, hwi.grand.total / .SD, .SDcols = colnames(hwi.mult.inds)]
+
+
+	for (j in 1:ncol(hwi.div)) set(hwi.div, which(is.infinite(hwi.div[[j]])), j, 0)
+
+	hwig <- hwi.div * hwi.dt
+	colnames(hwig) <- hwi.sums[, as.character(ID)]
+	return(hwig)
 }
