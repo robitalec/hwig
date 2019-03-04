65d1184a002c0af6a7db60baf4991f36fb9f1fe0
Alec Robitaille <robit.alec@gmail.com>
2019-03-04 14:16:54 -0800

    update and tidy calc_hwi

--- R/hwig.R
+++ R/hwig.R
@@ -1,65 +1,65 @@
 #' Calculate HWI
 #'
-#' @param ind.groups input group membership data, in ID/GROUP format
-#' @param input.data.format groups or individuals, as string (for get_group_by..)
-#' @param network.data.format  GBI or SP, group by individual or sampling period
-#' @param assc.ind SRI or HWI, for association_index
+#' @param DT input group membership data, in individual/group format
+#' @param id column indicating id in DT
+#' @param group column indicating group in DT
 #'
-#' @return data.table of HWI for each individual pair combination
+#' @return data.table of HWI
 #' @export
 #'
 #' @examples
-calc_hwi <- function(ind.groups, input.data.format = 'individuals',
-												 network.data.format = 'GBI', assc.ind = 'HWI'){
-	if (!'ID' %in% colnames(ind.groups)) {
-		warning("Column named 'ID' not found, ensure that an ID column is present in the input data")
-	}
-	if (!'GROUP' %in% colnames(ind.groups)) {
-		warning("Column named 'GROUP' not found, ensure that a GROUP column is present in the input data")
-	}
-	if (!is.character(input.data.format)) {
-		stop('require string input to all named variables (input.data.format)')
-	}
-	if (!is.character(network.data.format)) {
-		stop('require string input to all named variables (network.data.format)')
-	}
-	if (!is.character(assc.ind)) {
-		stop('require string input to all named variables (assc.ind)')
+calc_hwi <- function(DT, id, group) {
+	if (missing(DT)) stop('DT is missing')
+	if (missing(group)) stop('group is missing')
+	if (missing(id)) stop('id is missing')
+
+	if (!(group %in% colnames(DT))) {
+		stop('id column not found in DT')
 	}
 
-	# Process:
-	# Re-sort input data into group by individual with asnipe function
-	memb.matrix <- get_group_by_individual(ind.groups, data_format = input.data.format)
+	if (!(id %in% colnames(DT))) {
+		stop('id column not found in DT')
+	}
 
-	# Output HWI matrix into a data.table
-	hwi.full <- data.table(get_network(memb.matrix, data_format = network.data.format,
-																		 association_index = assc.ind))
-	return(hwi.full)
+	memb.matrix <- spatsoc::get_gbi(DT, group = group, id = id)
+
+	hwi <-
+		data.table::data.table(asnipe::get_network(
+			memb.matrix,
+			data_format = 'GBI',
+			association_index = 'HWI'
+		))
+	return(hwi)
 }
 
-
-#' @param hwi.dt output of `calc_hwi``
+#' Calculate HWIG
+#' @param hwi output of `calc_hwi``
 #'
 #' @return HWIG matrix
 #' @export
 #'
 #' @examples
-calc_hwig <- function(hwi.dt){
-	hwi.sums <- melt(hwi.dt[, lapply(.SD, sum), .SDcols = colnames(hwi.dt)],
-									 measure.vars = colnames(hwi.dt),
-									 variable.name = 'ID', value.name = 'HWI')
+calc_hwig <- function(hwi) {
+	hwi.sums <- melt(
+		hwi[, lapply(.SD, sum), .SDcols = colnames(hwi)],
+		measure.vars = colnames(hwi),
+		variable.name = 'ID',
+		value.name = 'HWI'
+	)
 
-	hwi.sums[, hwiTotal := sum(HWI)/2]
+	hwi.sums[, hwiTotal := sum(HWI) / 2]
 	hwi.grand.total <- hwi.sums[1, hwiTotal]
 
 	hwi.mult.inds <- data.table(hwi.sums[, outer(HWI, HWI, '*')])
 
-	hwi.div <- hwi.mult.inds[, hwi.grand.total / .SD, .SDcols = colnames(hwi.mult.inds)]
+	hwi.div <-
+		hwi.mult.inds[, hwi.grand.total / .SD, .SDcols = colnames(hwi.mult.inds)]
 
 
-	for (j in 1:ncol(hwi.div)) set(hwi.div, which(is.infinite(hwi.div[[j]])), j, 0)
+	for (j in 1:ncol(hwi.div))
+		set(hwi.div, which(is.infinite(hwi.div[[j]])), j, 0)
 
-	hwig <- hwi.div * hwi.dt
+	hwig <- hwi.div * hwi
 	colnames(hwig) <- hwi.sums[, as.character(ID)]
 	return(hwig)
 }
