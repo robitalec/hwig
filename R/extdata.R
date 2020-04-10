0aa9e2efe1ae59dc32cdd50698727eb0b436decc
Alec L. Robitaille <robit.alec@gmail.com>
2020-04-09 21:17:21 -0300

    rm junk extdata.R

--- R/extdata.R
+++ R/extdata.R
@@ -1,30 +1,3 @@
-# Load packages
-library(spatsoc)
-library(data.table)
-
-# Read example data
-DT <- fread(system.file("extdata", "DT.csv", package = "spatsoc"))
-
-# Cast the character column to POSIXct
-DT[, datetime := as.POSIXct(datetime, tz = 'UTC')]
-
-# Temporal grouping
-group_times(DT, datetime = 'datetime', threshold = '20 minutes')
-
-# Spatial grouping with timegroup
-group_pts(
-	DT,
-	threshold = 5,
-	id = 'ID',
-	coords = c('X', 'Y'),
-	timegroup = 'timegroup'
-)
-
-fwrite(DT[, .(id = ID, group, yr = year(datetime))],
-'inst/extdata/DT.csv')
-
-
-
 #' Example data for input to `hwig`
 #'
 #' @format A data.table with 14297 rows and 3 variables: \describe{
